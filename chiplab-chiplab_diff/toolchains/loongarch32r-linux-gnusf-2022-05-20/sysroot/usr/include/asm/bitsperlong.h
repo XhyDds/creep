9335a0940bb6d683466795409d4f399a693efe7b
Desert_Eagle <caochenrui@mail.ustc.edu.cn>
2023-07-13 19:39:20 +0800

    Revert "更新toolchains"
    This reverts commit 16831342239fca15ab2bad8e5de40d8ba18c8b21.

--- chiplab-chiplab_diff/toolchains/loongarch32r-linux-gnusf-2022-05-20/sysroot/usr/include/asm/bitsperlong.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef __ASM_LOONGARCH_BITSPERLONG_H
-#define __ASM_LOONGARCH_BITSPERLONG_H
-
-#define __BITS_PER_LONG _LOONGARCH_SZLONG
-
-#include <asm-generic/bitsperlong.h>
-
-#endif /* __ASM_LOONGARCH_BITSPERLONG_H */
