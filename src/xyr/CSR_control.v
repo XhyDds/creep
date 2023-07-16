f9551ef04d1b5a49f303b631801404cb8488a791
W34155552 <email@example.com>
2023-07-16 14:41:09 +0800

    部分完成CSR两周期访问

--- src/xyr/CSR_control.v
+++ src/xyr/CSR_control.v
@@ -4,7 +4,7 @@ parameter TLB_n=7,TLB_PALEN=32,TIMER_n=20
     input clk,rstn,
     input pipeline_CSR_stall,
     input pipeline_CSR_flush,
-    output CSR_pipeline_stall,
+    //output CSR_pipeline_stall,
     output CSR_pipeline_flush,
     output [31:0] CSR_pipeline_outpc,
     input [31:0] pipeline_CSR_inpc0,//
@@ -60,20 +60,20 @@ parameter TLB_n=7,TLB_PALEN=32,TIMER_n=20
     ADE='H8,ALE='H9,SYS='HB,BRK='HC,INE='HD,IPE='HE,FPD='HF,
     FPE='H12,TLBR='H3F;
     localparam ADEF='H0,ADEM='H1;
-    localparam Wait=0;
-    reg [4:0]ns,cs;
     reg [4:0] mode;wire [31:0] din;reg [31:0]dout,mask;
-    wire [8:0] ESTATin;reg busy,flushout;wire stallin,flushin;
+    wire [8:0] ESTATin;reg flushout;wire stallin,flushin;
     wire exe;wire [15:0] excp_arg1;reg clk_stall;reg [31:0] outpc;
     wire inte;wire [15:0] csr_num;reg [31:0] inpc;reg [5:0]ecode;reg [8:0] esubcode;
     reg [31:0] evaddr;wire [31:0]dwcsr;reg TI_cl;
-    reg [31:0] dwcsr_reg;
+    
+    reg [31:0] dwcsr_reg;reg flushout_reg;reg [31:0] outpc_reg;
+    reg [31:0] dout_reg;
     assign stallin=pipeline_CSR_stall,flushin=pipeline_CSR_flush;
-    assign CSR_pipeline_stall=busy,CSR_pipeline_flush=flushout;
+    assign CSR_pipeline_flush=flushout||flushout_reg;//CSR_pipeline_stall=busy,
     assign exe=pipeline_CSR_type==PRIV||excp_arg1[15]||(pipeline_CSR_type==LLW&&pipeline_CSR_subtype==LOAD);
     assign din=pipeline_CSR_din,CSR_pipeline_dout=dout;
     assign excp_arg1=pipeline_CSR_excp_arg1,CSR_pipeline_clk_stall=clk_stall;
-    assign CSR_pipeline_outpc=outpc,ESTATin=pipeline_CSR_ESTAT;
+    assign CSR_pipeline_outpc=outpc_reg,ESTATin=pipeline_CSR_ESTAT;
     assign csr_num=pipeline_CSR_excp_arg0;
     assign inte={ESTATin[8],TI_INTE,ESTATin[7:0],ESTAT_IS}&{ECFG_LIE[12:11],ECFG_LIE[9:0]}?CRMD[2]:0;
     always@(*)
@@ -93,12 +93,12 @@ parameter TLB_n=7,TLB_PALEN=32,TIMER_n=20
     always@(posedge(clk),negedge(rstn))
     begin
     if(!rstn||flushin)
-        begin
-        cs<=Wait;
+        begin   
+        
         end
-    else if(!stallin || busy)
+    else if(!stallin||inte)
         begin
-        cs<=ns;
+        
         end
     end
     always@(posedge(clk),negedge(rstn))
@@ -125,7 +125,7 @@ parameter TLB_n=7,TLB_PALEN=32,TIMER_n=20
     end
     always@(*)
     begin
-    busy=1;flushout=1;
+    flushout=1;
     outpc=inpc+4;dout=0;
     mask=~0;
     inpc=pipeline_CSR_inpc0;
@@ -133,7 +133,6 @@ parameter TLB_n=7,TLB_PALEN=32,TIMER_n=20
     esubcode=pipeline_CSR_excp_arg0[14:6];
     mode=pipeline_CSR_subtype;
     evaddr=pipeline_CSR_evaddr0;
-    ns=Wait;
     TI_cl=0;
     if(inte)
         begin
@@ -159,50 +158,44 @@ parameter TLB_n=7,TLB_PALEN=32,TIMER_n=20
         end
     if(ecode==ADE&&esubcode==ADEF)
         evaddr=inpc;
-    case(cs)
-        Wait:
-            begin
-            busy=0;
-            if((!stallin && !flushin && exe)||inte)
-                begin 
-                case(mode)
-                    ERTN:
-                        begin
-                        flushout=1;
-                        outpc=ERA;
-                        end
-                    CSRXCHG:
-                        begin
-                        flushout=1;                        
-                        mask=pipeline_CSR_mask;
-                        if(csr_num=='h44&&dwcsr[0])
-                            TI_cl=1;
-                        end
-                    INTE:
-                       begin
-                       flushout=1; 
-                       if(ecode==TLBR)
-                            begin
-                            outpc={TLBRENTRY,6'b0};
-                            end
-                       else
-                            outpc={EENTRY,6'b0};
-                       end 
-                    CSRWR:
-                         if(csr_num=='h44&&dwcsr[0])
-                            TI_cl=1;  
-                    CSRRD:
-                           flushout=0;     
-                endcase
-                end
-            else
+   
+    if((!stallin && !flushin && exe)||inte)
+        begin 
+        case(mode)
+            ERTN:
                 begin
-                flushout=0;
-                ns=Wait;
+                flushout=1;
+                outpc=ERA;
                 end
-            end
-    endcase
-    
+            CSRXCHG:
+                begin
+                flushout=1;                        
+                mask=pipeline_CSR_mask;
+                if(csr_num=='h44&&dwcsr[0])
+                    TI_cl=1;
+                end
+            INTE:
+               begin
+               flushout=1; 
+               if(ecode==TLBR)
+                    begin
+                    outpc={TLBRENTRY,6'b0};
+                    end
+               else
+                    outpc={EENTRY,6'b0};
+               end 
+            CSRWR:
+                 if(csr_num=='h44&&dwcsr[0])
+                    TI_cl=1;  
+            CSRRD:
+                   flushout=0;     
+        endcase
+        end
+    else
+        begin
+        flushout=0;
+        end
+
     case(csr_num)
         'h0:
             dout={23'b0,CRMD};
@@ -297,156 +290,143 @@ parameter TLB_n=7,TLB_PALEN=32,TIMER_n=20
         end
     else
         begin
-        case(cs)
-            Wait:
+            if((!stallin && !flushin && exe)||inte)
                 begin
-                if((!stallin && !flushin && exe)||inte)
+                if(mode==IDLE && !clk_stall)
                     begin
-                    if(mode==IDLE && !clk_stall)
-                        begin
-                        clk_stall<=1;
-                        end   
-                    else
-                        begin
-                        case(mode)
-                            ERTN:
-                                begin
-                                if(ESTAT_Ecode==TLBR)
-                                    CRMD[4:0]<={2'b10,PRMD};
-                                else
-                                    CRMD[4:0]<={2'b01,PRMD};
-                                LLBCTL_KLO<=0;
-                                if(!LLBCTL_KLO)
-                                    LLBCTL_ROLLB<=0;  
-                                end
-                            INTE:
-                                begin
-                                PRMD<=CRMD[2:0];
-                                CRMD[2:0]<=0;
-                                ERA<=inpc;
-                                clk_stall<=0;
-                                if(ecode==TLBR)
-                                    begin
-                                    CRMD[4:3]<=2'b01;
-                                    end
-                                ESTAT_Ecode<=ecode;
-                                ESTAT_EsubCode<=esubcode;
-                                BADV<=evaddr;
-                                end
-                            LLW:
-                                begin
-                                LLBCTL_ROLLB<=1;
-                                end
-                            CSRWR,CSRXCHG:
-                               case(csr_num)
-                                    'h0:
-                                        begin
-                                        CRMD[2:0]<=dwcsr[2:0];
-                                        CRMD[8:5]<=dwcsr[8:5];
-                                        if(dwcsr[4]^dwcsr[3])
-                                            CRMD[4:3]<=dwcsr[4:3];
-                                        end
-                                    'h1:
-                                        PRMD<=dwcsr[2:0];
-                                    'h2:
-                                        EUEN<=dwcsr[0];
-                                    'h4:
-                                        begin
-                                        ECFG_LIE[12:11]<=dwcsr[12:11];
-                                        ECFG_LIE[9:0]<=dwcsr[9:0];
-                                        end
-                                    'h5:
-                                        begin
-                                        ESTAT_IS<=dwcsr[1:0];
-                                        end
-                                    'h6:
-                                         ERA<=dwcsr;
-                                    'h7:
-                                        BADV<=dwcsr;
-                                    'hc:
-                                        EENTRY<=dwcsr[31:6];
-                                    'h10:
-                                        begin
-                                        TLBIDX_Index<=dwcsr[TLB_n-1:0];
-                                        TLBIDX_PS<=dwcsr[29:24];
-                                        TLBIDX_NE<=dwcsr[31];
-                                        end
-                                    'h11:
-                                        TLBEHI<=dwcsr[31:13];
-                                    'h12:
-                                        begin
-                                        TLBELO0_VDPLVMATG<=dwcsr[6:0];
-                                        TLBELO0_PPN<=dwcsr[TLB_PALEN-5:8];
-                                        end
-                                    'h13:
-                                        begin
-                                        TLBELO1_VDPLVMATG<=dwcsr[6:0];
-                                        TLBELO1_PPN<=dwcsr[TLB_PALEN-5:8];
-                                        end  
-                                    'h18:
-                                        begin
-                                        ASID_ASID<=dwcsr[9:0];
-                                        end
-                                    'h19:
-                                        PGDL<=dwcsr[31:12];
-                                    'h1a:
-                                        PGDH<=dwcsr[31:12];
-                                    //'h1b:
-                                    //'h20:
-                                        
-                                    'h30:
-                                        SAVE0<=dwcsr;
-                                    'h31:
-                                        SAVE1<=dwcsr;
-                                    'h32:
-                                        SAVE2<=dwcsr;
-                                    'h33:
-                                        SAVE3<=dwcsr;
-                                    'h40:
-                                        TID<=dwcsr;
-                                    'h41:
-                                        TCFG<=dwcsr[TIMER_n-1:0];
-                                    //'h42:
-                                    //'h44:    
-                                    'h60:
-                                        begin
-                                        if(dwcsr[1])
-                                            LLBCTL_ROLLB<=0;
-                                        LLBCTL_KLO<=dwcsr[2];
-                                        end
-                                    'h88:
-                                        TLBRENTRY<=dwcsr[31:6];
-                                    //'h98:  
-                                    'h180:
-                                        begin
-                                        DMW0_PLV0<=dwcsr[0];
-                                        DMW0_PLV3<=dwcsr[3];
-                                        DMW0_MAT<=dwcsr[5:4];
-                                        DMW0_PSEG<=dwcsr[27:25];
-                                        DMW0_VSEG<=dwcsr[31:29];
-                                        end
-                                    'h181:
-                                        begin
-                                        DMW1_PLV0<=dwcsr[0];
-                                        DMW1_PLV3<=dwcsr[3];
-                                        DMW1_MAT<=dwcsr[5:4];
-                                        DMW1_PSEG<=dwcsr[27:25];
-                                        DMW1_VSEG<=dwcsr[31:29];
-                                        end
-                                endcase 
-                        endcase
-                        end
-                    end
+                    clk_stall<=1;
+                    end   
                 else
                     begin
-                    
+                    case(mode)
+                        ERTN:
+                            begin
+                            if(ESTAT_Ecode==TLBR)
+                                CRMD[4:0]<={2'b10,PRMD};
+                            else
+                                CRMD[4:0]<={2'b01,PRMD};
+                            LLBCTL_KLO<=0;
+                            if(!LLBCTL_KLO)
+                                LLBCTL_ROLLB<=0;  
+                            end
+                        INTE:
+                            begin
+                            PRMD<=CRMD[2:0];
+                            CRMD[2:0]<=0;
+                            ERA<=inpc;
+                            clk_stall<=0;
+                            if(ecode==TLBR)
+                                begin
+                                CRMD[4:3]<=2'b01;
+                                end
+                            ESTAT_Ecode<=ecode;
+                            ESTAT_EsubCode<=esubcode;
+                            BADV<=evaddr;
+                            end
+                        LLW:
+                            begin
+                            LLBCTL_ROLLB<=1;
+                            end
+                        CSRWR,CSRXCHG:
+                           case(csr_num)
+                                'h0:
+                                    begin
+                                    CRMD[2:0]<=dwcsr[2:0];
+                                    CRMD[8:5]<=dwcsr[8:5];
+                                    if(dwcsr[4]^dwcsr[3])
+                                        CRMD[4:3]<=dwcsr[4:3];
+                                    end
+                                'h1:
+                                    PRMD<=dwcsr[2:0];
+                                'h2:
+                                    EUEN<=dwcsr[0];
+                                'h4:
+                                    begin
+                                    ECFG_LIE[12:11]<=dwcsr[12:11];
+                                    ECFG_LIE[9:0]<=dwcsr[9:0];
+                                    end
+                                'h5:
+                                    begin
+                                    ESTAT_IS<=dwcsr[1:0];
+                                    end
+                                'h6:
+                                     ERA<=dwcsr;
+                                'h7:
+                                    BADV<=dwcsr;
+                                'hc:
+                                    EENTRY<=dwcsr[31:6];
+                                'h10:
+                                    begin
+                                    TLBIDX_Index<=dwcsr[TLB_n-1:0];
+                                    TLBIDX_PS<=dwcsr[29:24];
+                                    TLBIDX_NE<=dwcsr[31];
+                                    end
+                                'h11:
+                                    TLBEHI<=dwcsr[31:13];
+                                'h12:
+                                    begin
+                                    TLBELO0_VDPLVMATG<=dwcsr[6:0];
+                                    TLBELO0_PPN<=dwcsr[TLB_PALEN-5:8];
+                                    end
+                                'h13:
+                                    begin
+                                    TLBELO1_VDPLVMATG<=dwcsr[6:0];
+                                    TLBELO1_PPN<=dwcsr[TLB_PALEN-5:8];
+                                    end  
+                                'h18:
+                                    begin
+                                    ASID_ASID<=dwcsr[9:0];
+                                    end
+                                'h19:
+                                    PGDL<=dwcsr[31:12];
+                                'h1a:
+                                    PGDH<=dwcsr[31:12];
+                                //'h1b:
+                                //'h20:
+                                    
+                                'h30:
+                                    SAVE0<=dwcsr;
+                                'h31:
+                                    SAVE1<=dwcsr;
+                                'h32:
+                                    SAVE2<=dwcsr;
+                                'h33:
+                                    SAVE3<=dwcsr;
+                                'h40:
+                                    TID<=dwcsr;
+                                'h41:
+                                    TCFG<=dwcsr[TIMER_n-1:0];
+                                //'h42:
+                                //'h44:    
+                                'h60:
+                                    begin
+                                    if(dwcsr[1])
+                                        LLBCTL_ROLLB<=0;
+                                    LLBCTL_KLO<=dwcsr[2];
+                                    end
+                                'h88:
+                                    TLBRENTRY<=dwcsr[31:6];
+                                //'h98:  
+                                'h180:
+                                    begin
+                                    DMW0_PLV0<=dwcsr[0];
+                                    DMW0_PLV3<=dwcsr[3];
+                                    DMW0_MAT<=dwcsr[5:4];
+                                    DMW0_PSEG<=dwcsr[27:25];
+                                    DMW0_VSEG<=dwcsr[31:29];
+                                    end
+                                'h181:
+                                    begin
+                                    DMW1_PLV0<=dwcsr[0];
+                                    DMW1_PLV3<=dwcsr[3];
+                                    DMW1_MAT<=dwcsr[5:4];
+                                    DMW1_PSEG<=dwcsr[27:25];
+                                    DMW1_VSEG<=dwcsr[31:29];
+                                    end
+                            endcase 
+                    endcase
                     end
                 end
-            
-            
-            
-            
-        endcase
         end
     end
     
