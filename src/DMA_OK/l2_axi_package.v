0be4a1150d6987be4546e6c11b0b8082b7bcb291
W34155552 <email@example.com>
2023-07-28 11:00:55 +0800

    加入AXI 文件

---
+++ src/DMA_OK/l2_axi_package.v
@@ -0,0 +1,298 @@
+/// 这里是l2_axi的接线预备
+/// l2对axi的访存行为主要有以下几种：
+/// 1.读操作：128位
+/// 2.写操作时，先读内存：128位
+/// 3.写脏时对内存写回：128位
+/// 4. 一周期只会发送一次访存req
+
+/// 尚未接入writebuffer
+/// 拟：读数据时
+///     先query writebuffer
+///     若结果为未命中，询问axi；
+///     否则，直接返回
+
+///   ibar会flush，所以不用担心icache没有访问到正确数据
+
+module l2_axi_package #(
+    offset_width=2
+)(
+    input      clk,rstn,
+    //l2 interface
+    //r
+    input      [31:0]addr_l2cache_mem_r,
+    output     [32*(1<<offset_width)-1:0]din_mem_l2cache,
+    input      l2cache_mem_req_r,
+    output     mem_l2cache_addrOK_r,
+    input      l2cache_mem_rdy,
+    output     mem_l2cache_dataOK,
+    //w
+    input      [31:0]addr_l2cache_mem_w,
+    input      [32*(1<<offset_width)-1:0]dout_l2cache_mem,
+    input      l2cache_mem_req_w,
+    output     mem_l2cache_addrOK_w, 
+    //直接访存标志
+    input      dma_sign,
+    input      [3:0]l2cache_axi_wstrb,
+
+    //AXI interface 
+    //read reqest
+    output   [ 3:0] arid,
+    output   [31:0] araddr,
+    output   [ 7:0] arlen,
+    output   [ 2:0] arsize,
+    output   [ 1:0] arburst,
+    output   [ 1:0] arlock,
+    output   [ 3:0] arcache,
+    output   [ 2:0] arprot,
+    output          arvalid,
+    input           arready,
+    //read back
+    input    [ 3:0] rid,
+    input    [31:0] rdata,
+    input    [ 1:0] rresp,
+    input           rlast,
+    input           rvalid,
+    output          rready,
+    //write request
+    output   [ 3:0] awid,
+    output   [31:0] awaddr,
+    output   [ 7:0] awlen,
+    output   [ 2:0] awsize,
+    output   [ 1:0] awburst,
+    output   [ 1:0] awlock,
+    output   [ 3:0] awcache,
+    output   [ 2:0] awprot,
+    output          awvalid,
+    input           awready,
+    //write data
+    output   [ 3:0] wid,
+    output   [31:0] wdata,
+    output   [ 3:0] wstrb,
+    output          wlast,
+    output          wvalid,
+    input           wready,
+    //write back
+    input    [ 3:0] bid,
+    input    [ 1:0] bresp,
+    input           bvalid,
+    output          bready
+);
+    wire [31:0]wrt_axi_waddr   ;
+    wire [31:0]wrt_axi_wdata   ;
+    wire wrt_axi_wvalid  ;
+    wire wrt_axi_wwvalid ;
+    wire axi_wrt_awready ;
+    wire axi_wrt_wready  ;
+    wire wrt_axi_wlast   ;
+    wire axi_wrt_bvalid  ;
+    wire wrt_axi_bready  ;
+
+    wire [31:0]addr_arbiter_wrt_w;
+    wire [(1<<offset_width)*32-1:0]dout_arbiter_wrt;
+    wire arbiter_wrt_req_w;
+    wire wrt_arbiter_addrOK_w;
+
+    wire l2_rready;
+    wire l2_rlast;
+    wire [31:0] l2_rdata;
+
+    wire l2_wvalid; 
+    wire l2_wwvalid;
+    wire l2_wready;
+    wire l2_waddrOK;
+    wire l2_wlast;
+    wire l2_bvalid;
+    wire l2_bready;
+    wire [31:0] l2_waddr;
+    wire [31:0] l2_wdata;
+
+    wire arbiter_mem_req;
+    wire mem_arbiter_dataOK;
+    wire [(1<<offset_width)*32-1:0] din_mem_arbiter;
+
+    wire [(1<<offset_width)*32-1:0] query_data;
+    wire query_ok;
+    wire cache_mem_rdy;
+    wire mem_arbiter_addrOK;
+
+    wire wrt_lock;
+    wire dma_lock;
+
+    wire [3:0]l2_wstrb;
+    wire [7:0]l2_wlen;
+    wire [7:0]l2_rlen;
+
+    ReturnBuffer#(
+        .offset_width       (offset_width)
+    )
+    l2cache_returnbuf(
+        .clk                (clk),
+        .rstn               (rstn),
+        .cache_mem_req      (arbiter_mem_req),
+        .mem_cache_dataOK   (mem_arbiter_dataOK),
+        .dout_mem_cache     (din_mem_arbiter),
+        .rready             (l2_rready),
+        .rdata              (l2_rdata),
+        // `ifdef L2Cache
+        .cache_mem_rdy      (cache_mem_rdy),
+        // `endif
+        .rlast              (l2_rlast)
+    );
+
+    WriteBuffer#(
+        .length         (5),
+        .offset_width   (offset_width)
+    )
+    l2cache_writebuffer(
+        .clk                (clk),
+        .rstn               (rstn),
+
+        .in_addr            (addr_arbiter_wrt_w),
+        .in_data            (dout_arbiter_wrt),
+        .in_valid           (arbiter_wrt_req_w),
+        .in_ready           (wrt_arbiter_addrOK_w),
+
+        .out_addr           (wrt_axi_waddr),
+        .out_data           (wrt_axi_wdata),
+        .out_valid          (wrt_axi_wvalid),
+        .out_wvalid         (wrt_axi_wwvalid),
+        .out_awready        (axi_wrt_awready),
+        .out_wready         (axi_wrt_wready),
+        .out_last           (wrt_axi_wlast),
+        .out_bvalid         (axi_wrt_bvalid),
+        .out_bready         (wrt_axi_bready),
+
+        .query_addr         (addr_l2cache_mem_r),
+        .query_data         (query_data),
+        .query_ok           (query_ok),
+
+        .dma_lock           (dma_lock),
+        .wrt_lock           (wrt_lock)
+    );
+
+    write_arbiter #(
+        .offset_width       (offset_width)
+    )
+    u_write_arbiter(
+        .clk                (clk),
+        .rstn               (rstn),
+        //l2cache_in
+        .addr_l2cache_mem_w (addr_l2cache_mem_w),
+        .dout_l2cache_mem   (dout_l2cache_mem),
+        .l2cache_mem_req_w  (l2cache_mem_req_w),
+        .mem_l2cache_addrOK_w(mem_l2cache_addrOK_w),
+        //l2cache_out
+        .l2_wstrb           (l2_wstrb),
+        .l2_len             (l2_wlen),
+        .l2_waddr           (l2_waddr),
+        .l2_wdata           (l2_wdata),
+        .l2_wvalid          (l2_wvalid),
+        .l2_wwvalid         (l2_wwvalid),
+        .l2_waddrOK         (l2_waddrOK),
+        .l2_wready          (l2_wready),
+        .l2_wlast           (l2_wlast),
+        .l2_bvalid          (l2_bvalid),
+        .l2_bready          (l2_bready),
+        //wrt_in
+        .addr_l2cache_wrt_w (addr_arbiter_wrt_w),
+        .dout_l2cache_wrt   (dout_arbiter_wrt),
+        .l2cache_wrt_req_w  (arbiter_wrt_req_w),
+        .wrt_l2cache_addrOK_w(wrt_arbiter_addrOK_w),
+        //wrt_out
+        .wrt_axi_addr       (wrt_axi_waddr),
+        .wrt_axi_data       (wrt_axi_wdata),
+        .wrt_axi_valid      (wrt_axi_wvalid),
+        .wrt_axi_wvalid     (wrt_axi_wwvalid),
+        .axi_wrt_awready    (axi_wrt_awready),
+        .axi_wrt_wready     (axi_wrt_wready),
+        .wrt_axi_last       (wrt_axi_wlast),
+        .axi_wrt_bvalid     (axi_wrt_bvalid),
+        .wrt_axi_bready     (wrt_axi_bready),
+        //直接访存
+        .l2cache_axi_wstrb  (l2cache_axi_wstrb),
+        .dma_sign           (dma_sign),
+
+        .dma_lock           (dma_lock),
+        .wrt_lock           (wrt_lock)
+    );
+
+    read_arbiter #(
+        .offset_width       (offset_width)
+    )
+    l2_read_arbiter(
+        .clk                (clk),
+        .rstn               (rstn),
+
+        .l2cache_mem_req_r  (l2cache_mem_req_r),
+        .mem_l2cache_addrOK_r(mem_l2cache_addrOK_r),
+        .l2cache_mem_rdy    (l2cache_mem_rdy),
+        .mem_l2cache_dataOK (mem_l2cache_dataOK),
+        .din_mem_l2cache    (din_mem_l2cache),
+
+        .query_data         (query_data),
+        .query_ok           (query_ok),
+
+        .l2_rlen            (l2_rlen),
+        .arbiter_mem_req    (arbiter_mem_req),
+        .mem_arbiter_addrOK (mem_arbiter_addrOK),
+        .mem_arbiter_dataOK (mem_arbiter_dataOK),
+        .dout_mem_arbiter   (din_mem_arbiter),
+        .cache_mem_rdy      (cache_mem_rdy),
+        .dma_sign           (dma_sign)
+    );
+
+    l2_axi_interface u_axi_interface(
+        //ports
+        .clk      		( clk      		),
+        .rstn     		( rstn     		),
+
+        //l2cache
+        .l2_rvalid 		( arbiter_mem_req ),//input       
+        .l2_raddrOK     ( mem_arbiter_addrOK),//output
+        .l2_rready 		( l2_rready 		),//output reg  
+        .l2_raddr  		( addr_l2cache_mem_r  		),//input [31:0]
+        .l2_rdata  		( l2_rdata  		),//output [31:0]
+        .l2_rlast  		( l2_rlast	),//output reg  
+        .l2_rlen        (l2_rlen),
+
+        .l2_wvalid 		( l2_wvalid         ),//input
+        .l2_wwvalid     ( l2_wwvalid        ),//input
+        .l2_waddrOK     ( l2_waddrOK        ),//output
+        .l2_wready 		( l2_wready 		),//output reg
+        .l2_waddr  		( l2_waddr  		),//input [31:0]
+        .l2_wdata  		( l2_wdata  		),//input [31:0]
+        .l2_wstrb  		( l2_wstrb   	    ),//input [3:0] 字节选通位
+        .l2_wlast  		( l2_wlast  		),//input
+        .l2_wlen        ( l2_wlen           ),
+
+        .l2_bvalid 		( l2_bvalid         ),//output reg
+        .l2_bready 		( l2_bready 		),//input
+        
+        //AXI
+        .araddr   		( araddr   		),
+        .arvalid  		( arvalid  		),
+        .arready  		( arready  		),
+        .arlen    		( arlen    		),
+        .arsize   		( arsize   		),
+        .arburst  		( arburst  		),
+        .rdata    		( rdata    		),
+        .rresp    		( rresp    		),
+        .rvalid   		( rvalid   		),
+        .rready   		( rready   		),
+        .rlast    		( rlast    		),
+        .awaddr   		( awaddr   		),
+        .awvalid  		( awvalid  		),
+        .awready  		( awready  		),
+        .awlen    		( awlen    		),
+        .awsize   		( awsize   		),
+        .awburst  		( awburst  		),
+        .wdata    		( wdata    		),
+        .wstrb    		( wstrb    		),
+        .wvalid   		( wvalid   		),
+        .wready   		( wready   		),
+        .wlast    		( wlast    		),
+        .bresp    		( bresp    		),
+        .bvalid   		( bvalid   		),
+        .bready   		( bready   		)
+    );
+endmodule
---
+++ src/DMA_OK/write_arbiter.v
@@ -0,0 +1,183 @@
+module write_arbiter#(
+    parameter   offset_width=2
+)(
+    input    clk,
+    input    rstn,
+    //l2cache_in
+    input    [31:0] addr_l2cache_mem_w,
+    input    [(1<<offset_width)*32-1:0]dout_l2cache_mem,
+    input    l2cache_mem_req_w,
+    output   reg  mem_l2cache_addrOK_w,
+    //l2cache_out
+    output   reg  [3:0]l2_wstrb,
+    output   reg  [7:0]l2_len,
+
+    output   reg  [31:0] l2_waddr,
+    output   reg  [31:0] l2_wdata,
+    output   reg  l2_wvalid,
+    output   reg  l2_wwvalid,
+    input    l2_waddrOK,
+    input    l2_wready,
+    output   reg  l2_wlast,
+    input    l2_bvalid,
+    output   reg  l2_bready,
+    //wrt_in
+    output   reg  [31:0] addr_l2cache_wrt_w,
+    output   reg  [(1<<offset_width)*32-1:0]dout_l2cache_wrt,
+    output   reg  l2cache_wrt_req_w,
+    input    wrt_l2cache_addrOK_w,
+    //wrt_out
+    input    [31:0] wrt_axi_addr,
+    input    [31:0] wrt_axi_data,
+    input    wrt_axi_valid,
+    input    wrt_axi_wvalid,
+    output   reg axi_wrt_awready,
+    output   reg axi_wrt_wready,
+    input    wrt_axi_last,
+    output   reg axi_wrt_bvalid,
+    input    wrt_axi_bready,
+    //直接访存
+    input    [3:0]l2cache_axi_wstrb,
+    input    dma_sign,
+    output   reg dma_lock,//dma锁
+    input    wrt_lock
+);
+    localparam IDLE = 3'd0,WRT_W = 3'd1,DMA_AW=3'd2 , DMA_W=3'd3 , DMA_R=3'd4;
+
+    reg [2:0] crt,nxt;
+    always @(posedge clk,negedge rstn) begin
+        if(!rstn) begin
+            crt <= IDLE;
+        end else begin
+            crt <= nxt;
+        end
+    end
+    always @(*) begin
+        case(crt)
+            IDLE: begin
+                if(l2cache_mem_req_w) begin
+                    if(dma_sign && wrt_lock==0)
+                                nxt = DMA_AW;
+                    else
+                                nxt = WRT_W;
+                end
+                else            nxt = IDLE;
+            end
+            WRT_W: begin
+                if(wrt_l2cache_addrOK_w)   
+                                nxt = IDLE;
+                else            nxt = WRT_W;
+            end
+            DMA_AW: begin
+                if(l2_waddrOK)   nxt = DMA_W;
+                else            nxt = DMA_AW;
+            end
+            DMA_W: begin
+                if(l2_wready)   nxt = DMA_R;
+                else            nxt = DMA_W;
+            end
+            DMA_R: begin
+                if(l2_bvalid)   nxt = IDLE;
+                else            nxt = DMA_R;
+            end
+            default:            nxt = IDLE;
+        endcase
+    end
+
+    always @(*) begin
+        mem_l2cache_addrOK_w=0;
+
+        l2_wstrb=0;
+        l2_len=0;
+        l2_waddr=0;
+        l2_wdata=0;
+        l2_wvalid=0;
+        l2_wwvalid=0;
+        l2_wlast=0;
+        l2_bready=0;
+
+        addr_l2cache_wrt_w=0;
+        dout_l2cache_wrt=0;
+        l2cache_wrt_req_w=0;
+
+        axi_wrt_awready=0;
+        axi_wrt_wready=0;
+        axi_wrt_bvalid=0;
+
+        dma_lock=0;
+        case (crt)
+            IDLE: begin
+                l2_len=(1<<offset_width)-1;
+                l2_wstrb=4'hF;
+
+                l2_waddr=wrt_axi_addr;
+                l2_wdata=wrt_axi_data;
+                l2_wvalid=wrt_axi_valid;
+                l2_wwvalid=wrt_axi_wvalid;
+                l2_wlast=wrt_axi_last;
+                l2_bready=wrt_axi_bready;
+
+                axi_wrt_awready=l2_waddrOK;
+                axi_wrt_wready=l2_wready;
+                axi_wrt_bvalid=l2_bvalid;
+
+                if(nxt==DMA_W) begin
+                    dma_lock=1;
+                end
+            end
+            WRT_W: begin
+                mem_l2cache_addrOK_w=wrt_l2cache_addrOK_w;
+
+                l2_len=(1<<offset_width)-1;
+                l2_wstrb=4'hF;
+
+                addr_l2cache_wrt_w=addr_l2cache_mem_w;
+                dout_l2cache_wrt=dout_l2cache_mem;
+                l2cache_wrt_req_w=l2cache_mem_req_w;
+
+                l2_waddr=wrt_axi_addr;
+                l2_wdata=wrt_axi_data;
+                l2_wvalid=wrt_axi_valid;
+                l2_wwvalid=wrt_axi_wvalid;
+                l2_wlast=wrt_axi_last;
+                l2_bready=wrt_axi_bready;
+
+                axi_wrt_awready=l2_waddrOK;
+                axi_wrt_wready=l2_wready;
+                axi_wrt_bvalid=l2_bvalid;
+
+            end
+            DMA_AW: begin
+                l2_wstrb=l2cache_axi_wstrb;
+                l2_len=8'd0;
+                l2_waddr=addr_l2cache_mem_w;
+                l2_wdata=dout_l2cache_mem[31:0];
+                l2_wvalid=1;
+                dma_lock=1;
+            end
+            DMA_W: begin
+                l2_wstrb=l2cache_axi_wstrb;
+                l2_len=8'd0;
+                l2_waddr=addr_l2cache_mem_w;
+                l2_wdata=dout_l2cache_mem[31:0];
+                l2_wvalid=1;
+                l2_wwvalid=1;
+                l2_wlast=1;
+
+                dma_lock=1;
+            end
+            DMA_R: begin
+                mem_l2cache_addrOK_w=l2_bvalid;
+
+                l2_wstrb=l2cache_axi_wstrb;
+                l2_len=8'd0;
+                l2_bready=1;
+
+                dma_lock=1;
+            end
+            default: ;
+        endcase
+    end
+
+
+endmodule
