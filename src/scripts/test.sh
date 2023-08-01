01fa6b04adbf7339be0ed08241f0f049cf1a06bc
XhyDds <1136759302@qq.com>
2023-08-01 22:14:57 +0800

    update scripts

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -5,9 +5,9 @@
 /bin/python3 ./writebuf.py
 /bin/python3 ./ghr.py
 /bin/python3 ./ras.py
-diff $PWD/Icache.v ../L1cache/Icache.v
-diff $PWD/Dcache.v ../L1cache/Dcache.v
-diff $PWD/L2cache.v ../L2cache/L2cache.v
+# diff $PWD/Icache.v ../L1cache/Icache.v
+# diff $PWD/Dcache.v ../L1cache/Dcache.v
+# diff $PWD/L2cache.v ../L2cache/L2cache.v
 diff $PWD/WriteBuffer.v ../AXI/WriteBuffer.v
-diff $PWD/ghr.v ../predictor/ghr.v
-diff $PWD/ras.v ../predictor/ras.v
+# diff $PWD/ghr.v ../predictor/ghr.v
+# diff $PWD/ras.v ../predictor/ras.v
