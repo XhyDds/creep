0afa95be9607716a14dba5d9b0782131b07187c1
Desert_Eagle <caochenrui@mail.ustc.edu.cn>
2023-08-01 10:47:50 +0800

    testend pc 1c000100

--- src/test/mycpu_tb_pref.v
+++ src/test/mycpu_tb_pref.v
@@ -40,7 +40,7 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 `define CONFREG_NUM_MONITOR  soc_lite.u_confreg.num_monitor
 `define CONFREG_UART_DISPLAY soc_lite.u_confreg.write_uart_valid
 `define CONFREG_UART_DATA    soc_lite.u_confreg.write_uart_data
-`define END_PC 32'hbfc00100
+`define END_PC 32'h1c000100
 
 module myCPU_tb( );
 reg resetn;
