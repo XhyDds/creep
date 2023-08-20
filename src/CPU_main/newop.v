0f461db9eedfa2458f2b871eb22355f4ad3476d1
Desert_Eagle <caochenrui@mail.ustc.edu.cn>
2023-08-20 13:06:58 +0800

    Update newop.v

--- src/CPU_main/newop.v
+++ src/CPU_main/newop.v
@@ -4,7 +4,7 @@ module newop (
     input      [31:0]rrd,rrk,ctr,
     output     [31:0]result,
     output     ifwrite,
-    output     stall,
+    output     stall
 );
     assign result=0;
     assign ifwrite=0;
