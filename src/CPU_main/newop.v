f2737856fa87b0c6626d0b4d561da098f05b72f7
W34155552 <email@example.com>
2023-08-20 15:04:45 +0800

    修复移位bug

--- src/CPU_main/newop.v
+++ src/CPU_main/newop.v
@@ -10,7 +10,7 @@ module newop (
     reg [5:0] num;wire [31:0]omask,mask,rrj_shift;
     wire [5:0] imma,immb,immc;
     
-    rotl rotl_mask(.src(omask),.shift(immb[4:0]),.result(mask));
+    rotl rotl_mask(.src(omask),.shift(immb[4:0]+1'b1),.result(mask));
     rotl rotl_rj(.src(rrj),.shift(imma[4:0]),.result(rrj_shift));
     assign result=rrj_shift&mask;
     
@@ -23,8 +23,8 @@ module newop (
     always@(*)
     begin
     if(immb>immc)
-        num=immc-immb+6'd32;
-    else if(immb>immc)
+        num=immc+(6'd32-immb);
+    else if(immb==immc)
         num=6'd0;
     else
         num=immc-immb;
@@ -40,6 +40,6 @@ module rotl(
     output [31:0] result
 );
 
-assign result=(src<<shift)|(src>>(32-shift));
+assign result=(src<<shift)|(src>>(6'd32-shift));
 
 endmodule
